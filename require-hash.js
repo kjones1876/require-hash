a46571fa99d45a74903c233679e844c71ae769d1
Kieran Jones <kjones1876@gmail.com>
2016-08-14 20:23:05 +0100

    Incorrect path issue

--- require-hash.js
+++ require-hash.js
@@ -1,14 +1,24 @@
+const stackTrace = require('stack-trace');
 const finder = require('find-package-json');
+const path = require('path');
 const Module = require('module');
 
 const originalRequire = Module.prototype.require;
 
 let LOCAL_SYMBOL = '#';
 
+function callerPath() {
+	return path.dirname(stackTrace.get()[4].getFileName());
+}
+
+function packagePath() {
+	return path.dirname(finder(callerPath()).next().value.__path);
+}
+
 Module.prototype.require = function require(..._args) {
 	const args = _args.concat([]);
 	if (args[0].substr(0, 1) === LOCAL_SYMBOL) {
-		args[0] = `${__dirname}/../../${args[0].substr(1)}`;
+		args[0] = `${packagePath()}/${args[0].substr(1)}`;
 	}
 
 	return originalRequire.apply(this, args);
